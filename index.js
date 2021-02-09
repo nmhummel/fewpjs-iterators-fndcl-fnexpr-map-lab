1b8b991f203949958f12845c565ca941a214ba00
nmhummel <natalie.hummel@gmail.com>
2021-02-08 23:24:09 -0600

    commit tree 26ec01bb957f93c5f46e2466e66bab6bfc3ed9d7

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map(title => {
+    let tokens = title.split(' ')
+    let capitalizedTokens =
+      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+    let response = capitalizedTokens.join(' ')
+    return response
+  })
 }
+
+
+
+
+// const activatedRobots = robots.map(function (robot) {
+//   return Object.assign({}, robot, {
+//     modes: robot.modes * 2,
+//     isActivated: true,
+//   });
+// });
+
+// const titleCased = tutorials.map(title => 
+// title //split then map 
+// then take each (word => //take the first letter(use .charAt) .toUpperCase() + rest of the word (use .slice), then join)
